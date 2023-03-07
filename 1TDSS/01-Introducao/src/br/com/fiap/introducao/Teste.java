45fc3119e4e6736b361360f464471647bee08882
Maria Eduarda Costa <63748897+Mecduda99@users.noreply.github.com>
2023-03-07 20:55:29 -0300

    Add files via upload

---
+++ 1TDSS/01-Introducao/src/br/com/fiap/introducao/Teste.java
@@ -0,0 +1,7 @@
+package br.com.fiap.introducao;
+
+public class Teste {
+	public static void main(String args[]) {
+		System.out.println("Ola Mundo");
+	}
+}
---
+++ 1TDSS/01-Introducao/src/br/com/fiap/introducao/Teste2.java
@@ -0,0 +1,25 @@
+package br.com.fiap.introducao;
+
+public class Teste2 {
+	public static void main(String args[]) {
+		System.out.println("Ola Mundo 2");
+	
+	//Tipos primitivos
+	//Logico
+	boolean estudando = true; //verdadeiro false //falso
+	//1 Caractere
+	char genero = 'f';
+	//Numeros Inteiros
+	byte idade = 10;
+	short alturaCm = 165;
+	int quantidade = 676554;
+	long populacao = 999993221;
+	//Numeros reais
+	float salario = 5000.50f; //f no final é float
+	double peso = 80.984;
+	
+	
+	
+	}	
+}
+
